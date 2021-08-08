58fda30533a162bf93b64bd2d77250351dc6a93b
Khinenw <deu07115@gmail.com>
2021-08-09 01:46:43 +0900

    revert(react): revert add makeUseI18n function
    This reverts commit 07f18c890edb68fff491f6177e92785a93c07136.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -125,9 +125,6 @@ export const useI18n = (componentI18n?: NamespacedI18nObject): UseI18n => {
 	return { t, ts };
 };
 
-export const makeUseI18n = (componentI18n: NamespacedI18nObject): () => UseI18n =>
-	() => useI18n(componentI18n);
-
 export type I18nContextType = {
 	i18n?: NamespacedI18nObject,
 	lang: I18nLang,
